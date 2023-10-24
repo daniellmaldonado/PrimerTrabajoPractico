eff248472b4bbf36f6142fc466867cb7ccd5ddba
Noelia <noeliaperazzo@gmail.com>
2023-10-24 19:57:42 -0300

    Agregué página de formulario de contacto y validación Javascript al archivo JS. Linkeé la nueva página a los menúes de navegación de las otras páginas.

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+/*----------------Consumo API---------------------*/
+
 const { createApp } = Vue
 
 
@@ -26,4 +28,42 @@ createApp({
         }
     }).mount('#app')
 
+/*----------------Validación de formulario---------------------*/
 
+function validar(){
+    let nombre = document.getElementById("nombre");
+    let email = document.getElementById("email");
+    let error = false;
+
+    document.getElementById("validar-nombre").innerHTML = "&nbsp;";
+    document.getElementById("validar-email").innerHTML = "&nbsp;";
+   
+    if (nombre.value == ""){
+        document.getElementById("validar-nombre").innerHTML = "Debe completar su nombre";
+        error = true;
+        nombre.focus();
+    }
+
+    if (email.value == ""){
+        document.getElementById("validar-email").innerHTML = "Debe completar su email";
+        error = true;
+        email.focus();
+    }
+
+    if(document.getElementById("exampleCheck1").checked == false){
+        document.getElementById("validar-condiciones").innerHTML = "Debe aceptar los terminos y condiciones";
+        error = true;
+    }
+
+
+    if (error == false){
+        document.getElementById("nombre").value = ""
+        document.getElementById("validar-nombre").innerHTML = "&nbsp;";
+        document.getElementById("email").value = ""
+        document.getElementById("validar-email").innerHTML = "&nbsp;";
+        
+        alert("Datos enviados")
+    }
+   return !error
+
+}
